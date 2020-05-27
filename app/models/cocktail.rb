dedc0236575b0caea6220a9ba35cab6776db6fd5
victoria vassileva <victoria.vassileva@yahoo.fr>
2020-05-27 16:05:09 +0200

    Seeding default cocktails to User

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,6 +3,6 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   belongs_to :user
 
-  validates :name, uniqueness: true, presence: true
+  validates :name, presence: true
   mount_uploader :photo, PhotoUploader
 end
